6faa782368f5830124b46da6354c916926feafc9
TMNhat1810 <minhnhat912002@gmail.com>
2024-11-18 10:45:58 +0700

    feat: set up oauth user and group

--- oauth/oauth/urls.py
+++ oauth/oauth/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from oauth2_provider import urls as oauth2_urls
+
+from .app.views import GroupList, UserDetails, UserList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("o/", include(oauth2_urls)),
+    path("users/", UserList.as_view()),
+    path("users/<pk>/", UserDetails.as_view()),
+    path("groups/", GroupList.as_view()),
 ]
